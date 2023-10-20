a451fc7e18e1282cef57d90a547d1397c0d1d838
chramb <79099832+chramb@users.noreply.github.com>
2023-10-20 22:19:16 +0200

    remove unused code

--- pystebin/database.py
+++ pystebin/database.py
@@ -1,56 +1,9 @@
-import logging
-from datetime import datetime
-
 import psycopg
 
-from pystebin.config import DBConfig, read_config
-from pystebin.models.paste import CONTENT_TYPE, Paste
-
-db_test = (
-    Paste(
-        id=1,
-        title="Hello",
-        content="aaaAAaaa...",
-        content_type=CONTENT_TYPE.Plaintext,
-        creation_time=datetime(2020, 10, 1),
-        author_id=79099832,
-    ),
-    Paste(
-        id=3,
-        title="Hello DIR SIR AAOMIMIKASNDIASDOASNDIASNDASNDOASD",
-        content="aaaAAaaa..." * 50,
-        content_type=CONTENT_TYPE.Plaintext,
-        creation_time=datetime(2020, 10, 1),
-        author_id=79099832,
-    ),
-    Paste(
-        id=4,
-        title="Hello",
-        content="aaaAAaaa...",
-        content_type=CONTENT_TYPE.Plaintext,
-        creation_time=datetime(2020, 10, 1),
-        author_id=79099832,
-    ),
-    Paste(
-        id=3,
-        title="Hello",
-        content="aaaAAaaa...",
-        content_type=CONTENT_TYPE.Plaintext,
-        creation_time=datetime(2020, 10, 1),
-        author_id=79099832,
-    ),
-)
+from pystebin.settings import DatabaseSettings
 
 
-def get(id: int) -> Paste | None:
-    for p in db_test:
-        if p.id == id:
-            return p
-
-    return None
-
-
-async def connect(config: DBConfig):
+async def connect(config: DatabaseSettings):
     con = await psycopg.AsyncConnection.connect(
         f"postgresql://{config.user}@{config.host}:{config.port}/{config.database}"
     )
@@ -60,15 +13,6 @@ async def connect(config: DBConfig):
         print(f"Connected with: {version[0]}") if version else None
     return con
 
-
-async def db():
-    config = read_config()
-    _db = await connect(config.db)
-    logging.warning("getting db connection")
-    try:
-        yield _db
-    finally:
-        await _db.close()
 
 async def init(con: psycopg.AsyncConnection):
     async with con.cursor() as cur:
